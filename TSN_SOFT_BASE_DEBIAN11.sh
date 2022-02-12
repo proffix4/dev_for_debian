4cb2921e464d18da5d341f44d89391c8a8f6dac8
Talipov S.N <proffix4@gmail.com>
2022-02-12 13:53:03 +0600

    Update TSN_SOFT_BASE_DEBIAN11.sh

--- TSN_SOFT_BASE_DEBIAN11.sh
+++ TSN_SOFT_BASE_DEBIAN11.sh
@@ -3,6 +3,6 @@
 sudo apt upgrade
 
 sudo apt install wget pavucontrol gthumb pdfshuffler qbittorrent gparted gtkhash obs-studio xfburn retext flameshot -y
-sudo apt install gammastep p7zip-full neofetch -y
+sudo apt install redshift-gtk p7zip-full neofetch -y
 
 sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
